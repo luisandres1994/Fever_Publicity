dcef6574f56c047a9566a7e61bea6b815c0d8066
andres <ramirezandres1994@gmail.com>
2017-04-24 09:38:31 -0400

    hiloo hora

--- src/fever_publicity/Fever_Publicity.java
+++ src/fever_publicity/Fever_Publicity.java
@@ -16,6 +16,8 @@ public class Fever_Publicity {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        clock C = new clock();
+       
     }
     
 }
